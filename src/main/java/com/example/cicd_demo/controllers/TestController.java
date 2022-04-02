5ad39137e99d87fd7b133fe296e94ddd7ecfb36a
itachi <annguyenvan.2k@gmail.com>
2022-04-02 10:46:17 +0700

    Test dev 12

--- src/main/java/com/example/cicd_demo/controllers/TestController.java
+++ src/main/java/com/example/cicd_demo/controllers/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello world ver 3";
+        return "Test sonarqube";
     }
 
     @GetMapping("/test")
