37379a67dd861b6f345da2aab0a9955fb10fb9f3
itachi <annguyenvan.2k@gmail.com>
2022-04-02 09:46:49 +0700

    test master 3

--- src/main/java/com/example/cicd_demo/controllers/TestController.java
+++ src/main/java/com/example/cicd_demo/controllers/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello world ver 2";
+        return "Hello world ver 3";
     }
 
     @GetMapping("/test")
